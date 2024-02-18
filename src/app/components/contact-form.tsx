092f77ec3f88e7861224dfc77d24feee19ddae4b
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-02-18 16:31:01 +0100

    Improve demo of default contact form

--- src/app/components/contact-form.tsx
+++ src/app/components/contact-form.tsx
@@ -1,12 +1,62 @@
 'use client';
 
 import { Widget } from '@typeform/embed-react';
+import { useEffect, useMemo, useState } from 'react';
 
 export default function ContactForm() {
+  const typeFormHiddenFields = {
+    pageLoadedAtDate: new Date().toISOString(),
+    randomNumberGeneratedOnPage: Math.random().toFixed(9),
+  };
+
+  const [typeFormState, setTypeFormState] = useState('Loading...');
+  const [questionChangeTime, setQuestionChangeTime] = useState<Date>();
+
+  useEffect(() => {
+    setQuestionChangeTime(new Date());
+  }, []);
+
+  const widgetMemo = useMemo(() => {
+    return (
+      <Widget
+        id='GPszre2l'
+        className='m-4 min-h-[600px] rounded border border-black'
+        opacity={75}
+        disableScroll={true}
+        enableSandbox={true}
+        autoResize={true}
+        hidden={typeFormHiddenFields}
+        onReady={() => setTypeFormState('Ready')}
+        onStarted={() => setTypeFormState('Started')}
+        onSubmit={() => setTypeFormState('Submitted')}
+        onClose={() => setTypeFormState('Closed')}
+        onEndingButtonClick={() => setTypeFormState('EndingButtonClicked')}
+        onQuestionChanged={() => setQuestionChangeTime(new Date())}
+      />
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div className='rounded border border-stone-400 bg-stone-300'>
+    <div className='rounded border border-stone-400 bg-amber-50'>
       <h2 className='p-4 text-xl font-bold'>Contact Form Example</h2>
-      <Widget id='GPszre2l' className='min-h-[600px] p-2' />
+      <p className='px-4'>
+        Note: the sandbox mode is enabled so no actual data is submitted or
+        saved.
+      </p>
+      <p className='mt-4 px-4'>
+        Typeform form is:
+        <span className='ml-1 inline-block rounded-xl border border-amber-300 bg-amber-200 px-2 font-bold text-amber-900'>
+          {typeFormState}
+        </span>
+      </p>
+      {widgetMemo}
+      <p className='mb-4 px-4'>
+        Last question change:
+        <span className='ml-1 inline-block rounded-xl border border-amber-300 bg-amber-200 px-2 font-bold text-amber-900'>
+          {questionChangeTime?.toISOString()?.substring(11, 23) || '-'}
+        </span>
+      </p>
     </div>
   );
 }
