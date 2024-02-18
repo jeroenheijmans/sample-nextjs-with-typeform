b3a17a30ac5fb01197e6d8d5947e3554960edb56
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-02-18 19:44:05 +0100

    Various updates to improve sample

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,14 +26,17 @@ export default function RootLayout({
             <div className='min-h-48'>{children}</div>
             <footer className='mt-auto w-full text-sm'>
               <div className='mx-auto mt-8 max-w-[800px]'>
-                <div className='border-t border-t-stone-400/50 p-4'>
+                <div className='flex gap-4 border-t border-t-stone-400/50 p-2'>
                   <Link
-                    className='text-indigo-800 underline hover:text-indigo-950'
+                    className='text-indigo-800 underline opacity-50 hover:text-indigo-950 hover:opacity-100'
                     href='https://github.com/jeroenheijmans/sample-nextjs-with-typeform'
                     target='_blank'
                   >
                     GitHub source
                   </Link>
+                  <span className='opacity-50'>
+                    Git commit {process.env.GIT_COMMIT_HASH}
+                  </span>
                 </div>
               </div>
             </footer>
